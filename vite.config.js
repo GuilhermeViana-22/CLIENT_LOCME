43f0193fa81e64ebffb46c6ca527c1b2cde3993f
Guilherme Viana <66335015+GuilhermeViana-22@users.noreply.github.com>
2025-06-09 15:08:01 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,42 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import compression from 'vite-plugin-compression'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+
   base: '/', // Se for um subdiretório, altere para '/meuapp/'
   css: ['~/assets/css/main.css'],
+  
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
+    compression({
+      threshold: 10240, // Comprime arquivos acima de 10KB
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+  preview: {
+    port: 3000,
+    host: true,
+    allowedHosts: ['www.bobflows.com']
+  },
+  define: {
+    __APP_ENV__: JSON.stringify(process.env.VITE_APP_ENV || 'development'),
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
+})
